ce7b9bca2932a7dbb588106e211612eb208b728a
Cappillen Lee <cappillen@ucsb.edu>
2025-07-01 11:56:47 -0700

    feat: error handle empty users, favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "spotify blender",
+  title: "blendify",
   description: "blend two spotify listeners anytime, anywhere",
 };
 
